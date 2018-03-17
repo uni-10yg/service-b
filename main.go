5290d15f2c63dbfc788ce0df3d2ccc4e750c3e23
ydu <>
2018-03-17 15:13:15 +0100

    ...

--- main.go
+++ main.go
@@ -8,24 +8,37 @@ import (
 	"github.com/gorilla/mux"
 	"golang.org/x/crypto/bcrypt"
 	"time"
+	"encoding/json"
 )
+
+type SaltedHash struct {
+	Hash	string	`json:"hash"`
+	Salt	int		`json:"salt"`
+}
 
 func randomFromRange(low, high int) int {
 	rand.Seed(time.Now().Unix())
 	return rand.Intn(high-low) + low
 }
 
-func hashAndSalt(phrase []byte) (string, int) {
-	var salt = randomFromRange(bcrypt.MinCost, bcrypt.MaxCost)
-	hash, err := bcrypt.GenerateFromPassword(phrase, salt)
+func hashAndSalt(phrase string) SaltedHash {
+	salt := randomFromRange(bcrypt.MinCost, bcrypt.MaxCost)
+	hash, err := bcrypt.GenerateFromPassword([]byte(phrase), salt)
 	if err != nil {
 		log.Println(err)
 	}
-	return string(hash), salt
+	salted_hash := SaltedHash{ string(hash), salt}
+	return salted_hash
 }
 
 func postPhrase(resp_writer http.ResponseWriter, request *http.Request) {
-	//params := mux.Vars(request)
+	params := mux.Vars(request)
+	salted_hash := hashAndSalt(params["phrase"])
+	//bytes, err := json.Marshal(salted_hash)
+	//if err != nil {
+	//	log.Println(err)
+	//}
+	json.NewEncoder(resp_writer).Encode(salted_hash)
 }
 
 func handleRequests() {

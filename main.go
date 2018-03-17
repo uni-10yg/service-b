02073c9c6c798f8e499d965f363c8be0b6cde9b9
ydu <>
2018-03-17 14:27:23 +0100

    added some functions

--- main.go
+++ main.go
@@ -3,18 +3,35 @@ package main
 import (
 	"log"
 	"net/http"
+	"math/rand"
 	//"encoding/json"
 	"github.com/gorilla/mux"
+	"golang.org/x/crypto/bcrypt"
+	"time"
 )
 
-func postCredentials(resp_writer http.ResponseWriter, request *http.Request) {
+func randomFromRange(low, high int) int {
+	rand.Seed(time.Now().Unix())
+	return rand.Intn(high-low) + low
+}
 
+func hashAndSalt(phrase []byte) (string, int) {
+	var salt = randomFromRange(bcrypt.MinCost, bcrypt.MaxCost)
+	hash, err := bcrypt.GenerateFromPassword(phrase, salt)
+	if err != nil {
+		log.Println(err)
+	}
+	return string(hash), salt
+}
+
+func postPhrase(resp_writer http.ResponseWriter, request *http.Request) {
+	//params := mux.Vars(request)
 }
 
 func handleRequests() {
 	router := mux.NewRouter()
-	router.HandleFunc("/", postCredentials).Methods("POST")
-	log.Fatal(http.ListenAndServe(":8081", router))
+	router.HandleFunc("/", postPhrase).Methods("POST")
+	log.Fatal(http.ListenAndServe("0.0.0.0:8081", router))
 }
 
 func main() {
